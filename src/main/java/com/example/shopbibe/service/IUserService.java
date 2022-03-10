1b99e8e9eaef054a8390bfcf4e0b4a1ce40c4b75
Nguyên Duy <duyiteahp@example.com>
2022-03-10 09:01:57 +0700

    cogi dau

--- src/main/java/com/example/shopbibe/service/IUserService.java
+++ src/main/java/com/example/shopbibe/service/IUserService.java
@@ -3,6 +3,8 @@ package com.example.shopbibe.service;
 
 
 import com.example.shopbibe.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
@@ -11,4 +13,6 @@ public interface IUserService {
     Boolean existsByUsername(String username); //username da co trong DB chua, khi tao du lieu
     Boolean existsByEmail(String email); //email da co trong DB chua
     User save(User user);
+
+
 }
